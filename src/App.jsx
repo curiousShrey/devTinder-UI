a824e25b23a2c581e388a5eb56eb7d77a86a9f9b
Shreyansh Joshi <shreyanshjoshi@Shreyanshs-MacBook-Pro.local>
2025-02-20 21:41:13 +0530

    Routing Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import  Body  from './Body';
+import Login from './Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>   
-      <h1>Hello World.</h1>
+    <>
+
+      <BrowserRouter basename='/'>
+          <Routes>
+           <Route path="/" element={<Body/>}>
+              <Route path='/login' element={<Login/>}/>
+           </Route>
+          </Routes>
+      </BrowserRouter>  
+      
     </>
   )
 }
