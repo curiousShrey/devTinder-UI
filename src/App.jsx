19a3d580fb5e0075965421603b5a4effd5db02d2
Shreyansh Joshi <shreyanshjoshi@Shreyanshs-MacBook-Pro.local>
2025-02-23 03:54:32 +0530

    Redux store added + Code structuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { useState } from 'react';
 
-import  Body  from './Body';
-import Login from './Login';
+import  Body  from './components/Body';
+import Login from './components/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 
 function App() {
 
   return (
     <>
-
-      <BrowserRouter basename='/'>
+    <Provider store={appStore}>
+     <BrowserRouter basename='/'>
           <Routes>
            <Route path="/" element={<Body/>}>
               <Route path='/login' element={<Login/>}/>
            </Route>
           </Routes>
       </BrowserRouter>  
+    </Provider>
       
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+
+  const user = useSelector((store) => store.userReducer)
+  console.log(user);
   return (
     <div className="navbar bg-base-100">
     <div className="flex-1">
       <a className="btn btn-ghost text-xl"> Dev Tinder</a>
     </div>
-    <div className="flex-none gap-2">
-      <div className="dropdown dropdown-end mx-7" >
+    {user && <div className="flex-none gap-1">
+      <p>Welcome, {user.firstName}</p>
+      <div className="dropdown dropdown-end mx-3" >
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
           <div className="w-10 rounded-full">
             <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              alt="User Photo"
+              src={user.photoUrl} />
           </div>
         </div>
         <ul
@@ -21,14 +26,12 @@ const Navbar = () => {
           <li>
             <a className="justify-between">
               Profile
-              <span className="badge">New</span>
             </a>
           </li>
-          <li><a>Settings</a></li>
           <li><a>Logout</a></li>
         </ul>
       </div>
-    </div>
+    </div>}
   </div>
   )
 }
