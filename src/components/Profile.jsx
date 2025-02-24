62482b9a1466baf24d87bbf3316b813e90a2ea03
Shreyansh Joshi <shreyanshjoshi@Shreyanshs-MacBook-Pro.local>
2025-02-24 12:48:57 +0530

    Profile component user state error fixed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,19 +8,19 @@ const Profile = () => {
     <div className='flex flex-col items-center mt-10'>
       <div className="avatar">
         <div className="w-24 rounded-full">
-          <img src={user?.photoUrl} />
+          <img src={user?.photoUrl || "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg" } />
         </div>
       </div>
 
       <div className='about-section p-5 text-xl'>
         <div className="card bg-primary text-primary-content w-96">
-          <div className="card-body">
+          {user && <div className="card-body">
             <h2 className="card-title">About</h2>
            <label>Name : {user.firstName} {user.lastName}</label>
            <label>Email ID : {user.emailId}</label>
            <label>Description : {user.about}</label>
            <label>Skills : {user.skills}</label>
-          </div>
+          </div>}
         </div>
       </div>
 
